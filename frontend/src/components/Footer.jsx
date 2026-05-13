3dae2a64a87f7946a9fc1b9edc9e2f9ef764d4f2
Codingwithriha <rihashehzadi2003@gmail.com>
2026-05-13 19:35:26 +0500

    Refactor Footer component for improved structure and content clarity

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,48 +1,46 @@
-import { assets } from "../assets/assets_frontend/assets.js";
+import React from 'react'
+import { assets } from '../assets/assets'
+
 const Footer = () => {
   return (
-    <div className="md:mx-10">
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
-        {/* --------------left section-------------- */}
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
         <div>
-          <img className="mb-5 w-40" src={assets.logo} alt="" />
-          <p className="w-full md:w-2/3 text-gray-600 leading-6">
-            Prescripto: Bridging the gap between doctors and patients with
-            seamless appointment management, secure prescriptions, and
-            personalized healthcare solutions. Your health, our priority.
-          </p>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
         </div>
 
-        {/* --------------center section-------------- */}
         <div>
-          <p className="text-xl font-medium mb-5">COMPANY</p>
-          <ul className="flex flex-col gap-2 text-gray-600">
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
             <li>Home</li>
             <li>About us</li>
-            <li>Contact us</li>
+            <li>Delivery</li>
             <li>Privacy policy</li>
           </ul>
         </div>
 
-        {/* --------------right section-------------- */}
         <div>
-          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
-          <ul className="flex flex-col gap-2 text-gray-600">
-            <li> Tel: (415) 555‑0132 </li>
-            <li>help@prescripto.com</li>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>greatstackdev@gmail.com</li>
           </ul>
         </div>
+
       </div>
+
       <div>
-        {/* -----------copy right text--------- */}
         <hr />
         <p className="py-5 text-sm text-center ">
           Copyright © {new Date().getFullYear()}{" "}
           <a className="hover:text-primary font-bold" href="https://www.linkedin.com/in/riha-shahzadi/">Riha Shahzadi</a>.
-        </p>
-      </div>
-    </div>
-  );
-};
+        </p>      
+        </div>
 
-export default Footer;
+    </div>
+  )
+}
+
+export default Footer
